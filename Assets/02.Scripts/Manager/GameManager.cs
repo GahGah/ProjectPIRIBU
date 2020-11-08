e90f9ba7289a0ced1bc00c1ed602344f5992f2b9
zpdl0703 <47559493+zpdl0703@users.noreply.github.com>
2020-11-08 20:28:24 +0900

    GameManager에 변수 추가: isDebugMode
    기능 플랫폼, 그래픽 리소스들의 렌더링 여부 체크용 / 혹은 기타 기능 디버그 시 확인용.
    
    자세한 사항은 내부 주석 확인 요망.

--- Assets/02.Scripts/Manager/GameManager.cs
+++ Assets/02.Scripts/Manager/GameManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameManager : SingleTon<GameManager>
 {
+    /* 
+     * "기능 플랫폼, 그래픽 리소스"들의 Start 함수에서 isDebugMode 변수 체크.
+     * if: isDebugMode == true 일 시,
+     * [기능 플랫폼].Renderer.enabled = true; [그래픽 리소스].Renderer.enabled = false;
+     * else:
+     * [기능 플랫폼].Renderer.enabled = false; [그래픽 리소스].Rnderer.enabled = true;
+     */
+
+    public bool isDebugMode = false;   
+
     public float timer = 0f;
 
     public InputManager inputManager;
