d8b4441fcdbd5b1b56d0b2363a6ddac9bc367da0
Lee Jae Hoon <70579980+cays75@users.noreply.github.com>
2020-11-29 21:42:00 +0900

    사다리 스프라이트 적용

--- Assets/02.Scripts/StageObject/Ladder.cs
+++ Assets/02.Scripts/StageObject/Ladder.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.WSA.Input;
+using UnityEngine.U2D;
 
 public class Ladder : InteractionObject {
 
+	public SpriteShapeController ladderShape;
 	protected override void Awake() {
 		base.Awake();
 		type = InteractionType.Ladder;
 		size = transform.localScale;
+		Spline spline = ladderShape.spline;
+
+		Vector3 scale = transform.localScale;
+		scale.x = 1;
+		transform.localScale = scale;
+		ladderShape.transform.localScale = new Vector3(1, 1 / scale.y, 1);
+		spline.SetPosition(0, Vector3.down * transform.localScale.y * 0.5f);
+		spline.SetPosition(1, Vector3.up * transform.localScale.y * 0.5f);
+
 	}
 
 
