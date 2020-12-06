97b4be11b791ce9c156363e11c986e0417af50cd
Lee Jae Hoon <70579980+cays75@users.noreply.github.com>
2020-12-06 15:04:48 +0900

    컷씬 페이지 애니메이션

---
+++ Assets/75.JaeHoonWork/CutScene/CutSceneManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneManager : MonoBehaviour
+{
+	public GameObject pagePrefab;
+	public List<Sprite> sprites;
+	private List<PageCurl> pages;
+	PageCurl page;
+	void Start()
+	{
+		pages = new List<PageCurl>();
+
+		//첫번째 페이지가 가장 마지막에 Instantiate되어야 가장 위에 보이게 되므로 Reverse
+		sprites.Reverse();
+		foreach (Sprite sprite in sprites) {
+			page = Instantiate(pagePrefab,transform).GetComponentInChildren<PageCurl>();
+			pages.Add(page);
+			page.SetSprite(sprite);
+		}
+
+		//첫 페이지가 마지막 item이므로 다시 Reverse시킴
+		pages.Reverse();
+		page = pages[0];
+
+	}
+
+	void Update()
+	{
+		if (InputManager.Instance.buttonMouseLeft.wasPressedThisFrame) {
+			
+			if (!pages[0]) {
+				pages.RemoveAt(0);
+				page = pages[0];
+			}
+
+			if (pages.Count > 1)
+				page.NextPage();
+		}
+	}
+}
