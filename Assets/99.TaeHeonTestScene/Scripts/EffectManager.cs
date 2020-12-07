c6fd70bfbd75d069201f0ce989862762b89e25ef
zpdl0703 <47559493+zpdl0703@users.noreply.github.com>
2020-12-07 12:29:01 +0900

    VFX 및 셰이더 작업 ing

---
+++ Assets/99.TaeHeonTestScene/Scripts/EffectManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EffectManager : SingleTon<GameManager>
+{
+    //접근만 하게 해 주는 매니저... 매니저가 맞나 이쯤되면?
+
+    [SerializeField]
+    public GameObject VFX_PipeSkill;
+}
