3773956db76ab9159da468846b3e6a0a26611197
JiWonHan <acsang12@gmail.com>
2020-12-13 23:45:31 +0900

    이펙트 매니저 살짝 수정

--- Assets/02.Scripts/Manager/EffectManager.cs
+++ Assets/02.Scripts/Manager/EffectManager.cs
@@ -6,7 +6,15 @@ using UnityEngine.SceneManagement;
 public class EffectManager : SingleTon<EffectManager>
 {
     //접근만 하게 해 주는 매니저... 매니저가 맞나 이쯤되면?
+    protected override void Awake()
+    {
+        base.Awake();
 
+    }
+    protected override void Init()
+    {
+        base.Init();
+    }
     [SerializeField]
     public GameObject VFX_PipeSkill;
 }
