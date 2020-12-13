ee5cca7631feafd1a1875ffe3428d09ecceb3400
Lee Jae Hoon <70579980+cays75@users.noreply.github.com>
2020-12-13 13:24:20 +0900

    로프 적용

---
+++ Assets/02.Scripts/StageObject/ExtendableWireShape.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[ExecuteInEditMode]
+public class ExtendableWireShape : MonoBehaviour
+{
+
+	public Transform fixTransform;
+	public SpriteShapeController shapeController;
+	Spline spline;
+
+	private void Awake() {
+		shapeController = GetComponent<SpriteShapeController>();
+	}
+	
+	void Update()
+	{
+		UpdateSpline();
+	}
+
+	void UpdateSpline() {
+		if (fixTransform && shapeController) {
+			spline = shapeController.spline;
+			Vector3 pos = fixTransform.position - transform.position;
+			pos.x = 0;
+			pos.z = 0;
+			spline.SetPosition(1, pos);
+		}
+	}
+}
