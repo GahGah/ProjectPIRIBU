c9b6a2a469213b5f6072059355c4b1b9a23edde0
JiWonHan <acsang12@gmail.com>
2020-12-15 07:38:01 +0900

    사운드 작업 중

--- Assets/75.JaeHoonWork/CutScene/CutSceneManager.cs
+++ Assets/75.JaeHoonWork/CutScene/CutSceneManager.cs
@@ -4,63 +4,73 @@ using UnityEngine;
 
 public class CutSceneManager : MonoBehaviour
 {
-	public GameObject pagePrefab;
-	public List<Sprite> sprites;
-	public string nextSceneName;
-	private List<PageCurl> pages;
-	PageCurl page;
-	bool disableNext;
-	void Start()
-	{
-		pages = new List<PageCurl>();
+    public GameObject pagePrefab;
+    public List<Sprite> sprites;
+    public string nextSceneName;
+    private List<PageCurl> pages;
+    PageCurl page;
+    bool disableNext;
+    void Start()
+    {
+        pages = new List<PageCurl>();
 
-		//첫번째 페이지가 가장 마지막에 Instantiate되어야 가장 위에 보이게 되므로 Reverse
-		sprites.Reverse();
-		foreach (Sprite sprite in sprites) {
-			page = Instantiate(pagePrefab,transform).GetComponentInChildren<PageCurl>();
-			pages.Add(page);
-			page.SetSprite(sprite);
-		}
+        //첫번째 페이지가 가장 마지막에 Instantiate되어야 가장 위에 보이게 되므로 Reverse
+        sprites.Reverse();
+        foreach (Sprite sprite in sprites)
+        {
+            page = Instantiate(pagePrefab, transform).GetComponentInChildren<PageCurl>();
+            pages.Add(page);
+            page.SetSprite(sprite);
+        }
 
-		//첫 페이지가 마지막 item이므로 다시 Reverse시킴
-		pages.Reverse();
-		page = pages[0];
-		disableNext = false;
-	}
+        //첫 페이지가 마지막 item이므로 다시 Reverse시킴
+        pages.Reverse();
+        page = pages[0];
+        disableNext = false;
+    }
 
-	void GoNextScene() {
-		if (!SceneChanger.Instance.isFading) {
-			SceneChanger.Instance.LoadScene(nextSceneName);
-			disableNext = true;
-		}
-	}
-	void Update()
-	{
-		if (!disableNext) {
-			
-			//ESC 스킵
-			if (InputManager.Instance.buttonPause.wasPressedThisFrame)
-			{
-				GoNextScene();
-			}
+    void GoNextScene()
+    {
+        if (!SceneChanger.Instance.isFading)
+        {
+            SceneChanger.Instance.LoadScene(nextSceneName);
+            disableNext = true;
+        }
+    }
+    void Update()
+    {
+        if (!disableNext)
+        {
 
-			if (InputManager.Instance.buttonMouseLeft.wasPressedThisFrame) {
-				
-				//넘긴 페이지 삭제
-				if (!pages[0]) {
-					pages.RemoveAt(0);
-					page = pages[0];
-				}
+            //ESC 스킵
+            if (InputManager.Instance.buttonPause.wasPressedThisFrame)
+            {
+                GoNextScene();
+            }
 
-				//다음 페이지
-				if (pages.Count > 1)
-					page.NextPage();
+            if (InputManager.Instance.buttonMouseLeft.wasPressedThisFrame)
+            {
 
-				//마지막 페이지면 다음씬으로
-				else
-					GoNextScene();
-			}
+                //넘긴 페이지 삭제
+                if (!pages[0])
+                {
+                    pages.RemoveAt(0);
+                    page = pages[0];
+                }
 
-		}
-	}
+                //다음 페이지
+                if (pages.Count > 1)
+                {
+                   
+                    page.NextPage();
+                    SoundManager.Instance.PlaySFXOneShot(SoundManager.Instance.audioClips[2]);
+                }
+
+                //마지막 페이지면 다음씬으로
+                else
+                    GoNextScene();
+            }
+
+        }
+    }
 }
